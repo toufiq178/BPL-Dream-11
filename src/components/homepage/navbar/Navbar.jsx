850a4b0b9f2e37d4c0a268730b5e0a45ef573912
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-24 19:34:35 +0600

    navbar added

---
+++ src/components/homepage/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import cricketLogo from "../../../assets/logo.png";
+import dollarImg from "../../../assets/dollar_1.png"
+
+const Navbar = () => {
+  return (
+    <div class="navbar bg-base-100 shadow-sm px-10">
+      <div class="navbar-start">
+        <img className="w-12 h-12" src={cricketLogo} alt="" />
+      </div>
+      <div class="navbar-end hidden lg:flex">
+        <ul class="menu menu-horizontal px-1 flex gap-4">
+          <li>
+            <a>Home</a>
+          </li>
+
+          <li>
+            <a>Fixture</a>
+          </li>
+          <li>
+            <a>Teams</a>
+          </li>
+          <li>
+            <a>Schedules</a>
+          </li>
+        </ul>
+        <div class="">
+          <div class="btn flex justify-between items-center gap-2 text-xl font-bold"> 0 Coin <img src={dollarImg} alt="" /></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
